ac208c1bc799db026011ac14f78d9e605f355706
Abdesch <147941641+Abdesch@users.noreply.github.com>
2025-02-24 20:36:33 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Le site est chargé !");
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionSections = document.querySelectorAll('.accordion-section');
+
+    accordionSections.forEach(section => {
+        section.addEventListener('toggle', function() {
+            if (this.open) { // Si cette section est ouverte
+                // Fermer toutes les autres sections
+                accordionSections.forEach(otherSection => {
+                    if (otherSection !== this && otherSection.open) {
+                        otherSection.open = false;
+                    }
+                });
+            }
+        });
+    });
 });
