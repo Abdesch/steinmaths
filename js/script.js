71e567eef87a81823102dbf8eeb936e918946c44
Abdesch <147941641+Abdesch@users.noreply.github.com>
2025-02-25 16:21:13 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,24 +13,4 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
-
-    document.querySelectorAll('.accordion-section').forEach(accordionSection => {
-        const summaryElement = accordionSection.querySelector('summary');
-        const downloadLinksElement = accordionSection.querySelector('.download-links');
-
-        // Afficher les liens par défaut seulement pour le premier élément
-        if (accordionSection === accordionSections[0] && !accordionSection.querySelector('details').open ) {
-            downloadLinksElement.style.display = 'block';
-        } else {
-            downloadLinksElement.style.display = 'none';
-        }
-
-        accordionSection.addEventListener('toggle', function() {
-            if (this.open) {
-                downloadLinksElement.style.display = 'block';
-            } else {
-                downloadLinksElement.style.display = 'none';
-            }
-        });
-    });
 });
