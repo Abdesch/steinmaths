cf0390eca7e7ea0b23c44d99eea9f2099be76bde
Abdesch <147941641+Abdesch@users.noreply.github.com>
2025-02-25 00:42:54 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const accordionSections = document.querySelectorAll('.accordion-section');
+   <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        const accordionSections = document.querySelectorAll('.accordion-section');
 
-    accordionSections.forEach(section => {
-        section.addEventListener('toggle', function() {
-            if (this.open) { // Si cette section est ouverte
-                // Fermer toutes les autres sections
-                accordionSections.forEach(otherSection => {
-                    if (otherSection !== this && otherSection.open) {
-                        otherSection.open = false;
-                    }
-                });
-            }
+        accordionSections.forEach(section => {
+            section.addEventListener('toggle', function () {
+                if (this.open) { // Si cette section est ouverte
+                    // Fermer toutes les autres sections
+                    accordionSections.forEach(otherSection => {
+                        if (otherSection !== this && otherSection.open) {
+                            otherSection.open = false;
+                        }
+                    });
+                }
+            });
         });
     });
-});
+   script>
