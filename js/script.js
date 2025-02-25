9f9fa88750f690ba13ce72e73d17a94d97b0c444
Abdesch <147941641+Abdesch@users.noreply.github.com>
2025-02-25 15:57:18 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,22 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    document.querySelectorAll('.accordion-section').forEach(accordionSection => {
-        const summaryElement = accordionSection.querySelector('summary');
-        const downloadLinksElement = accordionSection.querySelector('.download-links');
-
-        // Afficher les liens par défaut seulement pour le premier élément
-        if (accordionSection === accordionSections[0] && !accordionSection.querySelector('details').open ) {
-            downloadLinksElement.style.display = 'block';
-        } else {
-            downloadLinksElement.style.display = 'none';
+    // Obtenir toutes les sections "details" et les liens de téléchargement
+    const detailsElements = document.querySelectorAll('.accordion-section');
+    detailsElements.forEach(detailsElement => {
+        // Vérifier si l'élément courant n'est pas le premier
+        if (!detailsElement.matches(':first-of-type')) {
+            // Cacher l'élément
+            detailsElement.style.display = 'none';
         }
-
-        accordionSection.addEventListener('toggle', function() {
-            if (this.open) {
-                downloadLinksElement.style.display = 'block';
-            } else {
-                downloadLinksElement.style.display = 'none';
+        
+        // Si il clique sur l'element il s'affiche
+        detailsElement.addEventListener('toggle', function() {
+            // Vérifier si l'élément courant est bien la section à enlever
+            if (!detailsElement.matches(':first-of-type')) {
+                 if (detailsElement.style.display === 'none'){
+                     detailsElement.style.display = 'block';
+                 } else {
+                     detailsElement.style.display = 'none';
+                 }
             }
         });
     });
